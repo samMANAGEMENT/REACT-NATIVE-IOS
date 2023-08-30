e29028af63e9f5e9dc96f4eedb10780f6954c348
Samuel Pineda <97739674+samMANAGEMENT@users.noreply.github.com>
2023-08-30 01:47:18 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import "react-native-gesture-handler";
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import NavigationStack from "./src/navigation/NavigationStack";
+import NavigationTab from "./src/navigation/NavigationTab";
+import NavigationDrawer from "./src/navigation/NavigationDrawer";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      {/* <NavigationStack /> */}
+      {/* <NavigationTab /> */}
+      <NavigationDrawer />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
